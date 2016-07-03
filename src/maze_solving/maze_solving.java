111265ad6116168cd3cece1d7b02632eb8b731bf
mohammed-ridwanul <mr3islam@uwaterloo.ca>
2016-07-03 08:04:45 -0400

    Added A* search Need to fix DFS and BFS

--- src/maze_solving/maze_solving.java
+++ src/maze_solving/maze_solving.java
@@ -4,13 +4,20 @@ import java.util.*;
 
 public class maze_solving {
 	static LinkedList<node> myQueue = new LinkedList<node>();
+	
 	static int depth;
 	static int size;		
 	static boolean flag = false;
+	static int gRow;
+	static int gCol;
 	
 	//Maze is initialized to 0 by default
 	static int[][] maze;
+	static int[][] temp_maze;
+	
 	static ArrayList<node> path_array = new ArrayList<node>();
+	static ArrayList<node> a_star_array = new ArrayList<node>();
+	static LinkedList <node> actual_path = new LinkedList<node>();
 	
 	public static void main(String[] args) {		
 		int i = 0;
@@ -46,92 +53,201 @@ public class maze_solving {
 		maze[row][col] = 1;
 		
 		System.out.print("Enter goal row: ");
-		int gRow = Integer.parseInt(scanner.next());
+		gRow = Integer.parseInt(scanner.next());
 		System.out.print("Enter goal column: ");
-		int gCol = Integer.parseInt(scanner.next());
+		gCol = Integer.parseInt(scanner.next());
 		scanner.close();
 		
 		maze[gRow][gCol] = 2;
 		depth = 0;
+		temp_maze = maze.clone();
 		
 		Long t1 = System.nanoTime();
 		
 		node tmp = create_node(row,col,0);
 		
-		/* 												Depth First Search 
-		myQueue.addFirst(tmp);
-		depthFirstSearch(tmp);
-		 												Depth First Search End 
-		*/
+//		Depth First Search 
+//		myQueue.addFirst(tmp);
+//		depthFirstSearch(tmp);
 		
-		/* Breadth First Search */
-//		myQueue.addLast(tmp);
-		breadthFirstSearch(tmp);
+//		Breadth First Search
+//		breadthFirstSearch(tmp);
+		
+//		A* Search
+		a_star(tmp, 0);
 		
 		Long t2 = System.nanoTime() - t1;
 		
 		for(node n : path_array){
-			System.out.println("(" + n.getRow() + " " + n.getColumn() + ")" );
+			System.out.println("(" + n.y() + " " + n.x() + ")" );
 		}
 		
 		System.out.println("Found path: " + flag);
-		System.out.println("Total cost: " + depth);
+		//All others
+//		System.out.println("Total cost: " + depth);
+		
+		//A*star
+		System.out.println("Total cost: " + actual_path.size());
 		
 		System.out.println("Elapsed time: " + t2 + " ns");
 		
+	}
+	
+	public static int a_star(node c_node, int depth){
+		
+		//Found goal
+		if(maze[c_node.y()][c_node.x()] == 2){
+			flag = true;
+			return 1;
+		}
+		
+		gen_neighbours(c_node, c_node.getDepth());
+		
+		//TODO check if empty
+		if(a_star_array.isEmpty()){
+			flag = false;
+			return 0;
+		}
+		
+		node tmp = a_star_array.remove(0);
+	
+		clear_path();
+		append_path(tmp);
+		
+		return(a_star(tmp, tmp.getDepth()));
+	}
+	
+	private static void clear_path() {
+		//Resets maze to original, allowing backtracking. 
+		temp_maze = maze.clone();
+		actual_path.clear();
+	}
+
+	private static void append_path(node temp) {
+		//Set visited
+		maze[temp.y()][temp.x()] = 9;
+		if(temp.getParent() == null){
+			return;
+		}else{
+			actual_path.add(temp);
+			append_path(temp.getParent());
+		}
+	}
+
+	private static boolean gen_neighbours(node c_node, int d) {
+		boolean hasChild = false;
+		
+		//Check left
+		if(c_node.x()>= 1){
+			if(maze[c_node.y()][c_node.x() - 1] != 9 || maze[c_node.y()][c_node.x() - 1] != 3 ){
+				node left = create_node(c_node.y(), c_node.x()-1, (d+1));
+				left.setEval(eval(c_node.y(), c_node.x()-1, (d+1)));
+				left.setParent(c_node);
+				a_star_array.add(left);
+				hasChild = true;
+			}
+		}
+		
+		//Down
+		if(c_node.y() < size - 1 ){
+			if(maze[c_node.y() + 1][c_node.x()] != 9 || maze[c_node.y() + 1][c_node.x()] != 3 ){
+				node down = create_node(c_node.y() + 1, c_node.x(), d+1 );
+				down.setEval(eval(c_node.y() + 1, c_node.x(), d+1));
+				down.setParent(c_node);
+				a_star_array.add(down);
+				hasChild = true;
+			}
+		}
+		
+		//right
+		if(c_node.x() < size-1 ){
+			if(maze[c_node.y()][c_node.x() + 1] != 9 || maze[c_node.y()][c_node.x() + 1] != 3 ){
+				node right = create_node(c_node.y(), c_node.x() + 1, d+1 );
+				right.setEval(eval(c_node.y(), c_node.x() + 1, d+1 ));
+				right.setParent(c_node);
+				a_star_array.add(right);
+				hasChild = true;
+			}
+		}
+		
+		//Up
+		if(c_node.y() >= 1 ){
+			if(maze[c_node.y() - 1][c_node.x()] != 9 || maze[c_node.y() - 1][c_node.x()] != 3 ){
+				node up = create_node(c_node.y()-1, c_node.x(), d+1 );
+				up.setEval(eval(c_node.y()-1, c_node.x(), d+1 ));
+				up.setParent(c_node);
+				a_star_array.add(up);
+				hasChild = true;
+			}
+		}
+		if(hasChild){
+			Collections.sort(a_star_array);
+		}
+		
+		return hasChild;
+	}
+
+	public static int eval(int row, int column, int depth){
+		//Use Manhattan distance for heuristic 
+		int heurestic = Math.abs(gRow - row) + Math.abs(gCol - column);
+		return (heurestic+depth);
 	}
 	
 	public static int breadthFirstSearch(node c_node){
 		path_array.add(c_node);
 		
 		//Found goal
-		if(maze[c_node.getRow()][c_node.getColumn()] == 2){
+		if(maze[c_node.y()][c_node.x()] == 2){
 			flag = true;
 			return 1;
 		}
 		
 		//Set visited
-		maze[c_node.getRow()][c_node.getColumn()] = 9; 
+		maze[c_node.y()][c_node.x()] = 9; 
 		
 
 		//Left
-		if(c_node.getColumn()>= 1){
-			if(maze[c_node.getRow()][c_node.getColumn() - 1] == 0 || maze[c_node.getRow()][c_node.getColumn() - 1] == 2 ){
-				node left = create_node(c_node.getRow(), c_node.getColumn()-1, depth+1 );
+		if(c_node.x()>= 1){
+			if(maze[c_node.y()][c_node.x() - 1] == 0 || maze[c_node.y()][c_node.x() - 1] == 2 ){
+				node left = create_node(c_node.y(), c_node.x()-1, depth+1 );
 				myQueue.addLast(left);
 			}
 		}
 		
 		//down
-		if(c_node.getRow() < size - 1 ){
-			if(maze[c_node.getRow() + 1][c_node.getColumn()] == 0 || maze[c_node.getRow() + 1][c_node.getColumn()] == 2 ){
-				node down = create_node(c_node.getRow() + 1, c_node.getColumn(), depth+1 );
+		if(c_node.y() < size - 1 ){
+			if(maze[c_node.y() + 1][c_node.x()] == 0 || maze[c_node.y() + 1][c_node.x()] == 2 ){
+				node down = create_node(c_node.y() + 1, c_node.x(), depth+1 );
 				myQueue.addLast(down);
 			}
 		}
 		
 		//right
-		if(c_node.getColumn() < size-1 ){
-			if(maze[c_node.getRow()][c_node.getColumn() + 1] == 0 || maze[c_node.getRow()][c_node.getColumn() + 1] == 2 ){
-				node right = create_node(c_node.getRow(), c_node.getColumn() + 1, depth+1 );
+		if(c_node.x() < size-1 ){
+			if(maze[c_node.y()][c_node.x() + 1] == 0 || maze[c_node.y()][c_node.x() + 1] == 2 ){
+				node right = create_node(c_node.y(), c_node.x() + 1, depth+1 );
 				myQueue.addLast(right);
 			}
 		}
 		
 		//Up
-		if(c_node.getRow() >= 1 ){
-			if(maze[c_node.getRow() - 1][c_node.getColumn()] == 0 || maze[c_node.getRow() - 1][c_node.getColumn()] == 2 ){
-				node up = create_node(c_node.getRow()-1, c_node.getColumn(), depth+1 );
+		if(c_node.y() >= 1 ){
+			if(maze[c_node.y() - 1][c_node.x()] == 0 || maze[c_node.y() - 1][c_node.x()] == 2 ){
+				node up = create_node(c_node.y()-1, c_node.x(), depth+1 );
 				myQueue.addLast(up);
 			}
 		}
+
 		if(myQueue.isEmpty()){
 			return 0;
 		}
 		
+		depth++;
+		
 		if(breadthFirstSearch(myQueue.removeFirst()) == 1){
 			return 1;
 		}else{
+			depth--;
 			return 0;
 		}
 	}
@@ -148,64 +264,74 @@ public class maze_solving {
 		myQueue.removeFirst();
 		
 		//Found goal
-		if(maze[c_node.getRow()][c_node.getColumn()] == 2){
+		if(maze[c_node.y()][c_node.x()] == 2){
 			flag = true;
 			return 1;
 		}
 		
 		//Set visited
-		maze[c_node.getRow()][c_node.getColumn()] = 9; 
+		maze[c_node.y()][c_node.x()] = 9; 
 		
 
 		//Left
-		if(c_node.getColumn()>= 1){
-			if(maze[c_node.getRow()][c_node.getColumn() - 1] == 0 || maze[c_node.getRow()][c_node.getColumn() - 1] == 2 ){
-				node left = create_node(c_node.getRow(), c_node.getColumn()-1, depth+1 );
+		if(c_node.x()>= 1){
+			if(maze[c_node.y()][c_node.x() - 1] == 0 || maze[c_node.y()][c_node.x() - 1] == 2 ){
+				node left = create_node(c_node.y(), c_node.x()-1, depth+1 );
 				myQueue.addFirst(left);
 //				depth++;
 				if(depthFirstSearch(left)==1){
 					return 1;
-				};				
+				};	
+				System.out.println(path_array.get(path_array.size()-1));
+				path_array.remove(path_array.size()-1);
+				System.out.println(path_array.get(path_array.size()));
 			}
 		}
 		
 		//down
-		if(c_node.getRow() < size - 1 ){
-			if(maze[c_node.getRow() + 1][c_node.getColumn()] == 0 || maze[c_node.getRow() + 1][c_node.getColumn()] == 2 ){
-				node down = create_node(c_node.getRow() + 1, c_node.getColumn(), depth+1 );
+		if(c_node.y() < size - 1 ){
+			if(maze[c_node.y() + 1][c_node.x()] == 0 || maze[c_node.y() + 1][c_node.x()] == 2 ){
+				node down = create_node(c_node.y() + 1, c_node.x(), depth+1 );
 				myQueue.addFirst(down);
 //				depth++;
 				if(depthFirstSearch(down)==1){
 					return 1;
 				};
+				System.out.println(path_array.get(path_array.size()-1));
+				path_array.remove(path_array.size()-1);
 			}
 		}
 		
 		//right
-		if(c_node.getColumn() < size-1 ){
-			if(maze[c_node.getRow()][c_node.getColumn() + 1] == 0 || maze[c_node.getRow()][c_node.getColumn() + 1] == 2 ){
-				node right = create_node(c_node.getRow(), c_node.getColumn() + 1, depth+1 );
+		if(c_node.x() < size-1 ){
+			if(maze[c_node.y()][c_node.x() + 1] == 0 || maze[c_node.y()][c_node.x() + 1] == 2 ){
+				node right = create_node(c_node.y(), c_node.x() + 1, depth+1 );
 				myQueue.addFirst(right);
 //				depth++;
 				if(depthFirstSearch(right) ==1){
 					return 1;
 				}; 
+				System.out.println(path_array.get(path_array.size()-1));
+				path_array.remove(path_array.size()-1);
 			}
 		}
 		
 		//Up
-		if(c_node.getRow() >= 1 ){
-			if(maze[c_node.getRow() - 1][c_node.getColumn()] == 0 || maze[c_node.getRow() - 1][c_node.getColumn()] == 2 ){
-				node up = create_node(c_node.getRow()-1, c_node.getColumn(), depth+1 );
+		if(c_node.y() >= 1 ){
+			if(maze[c_node.y() - 1][c_node.x()] == 0 || maze[c_node.y() - 1][c_node.x()] == 2 ){
+				node up = create_node(c_node.y()-1, c_node.x(), depth+1 );
 				myQueue.addFirst(up);
 //				depth++;
 				if(depthFirstSearch(up) == 1){
 					return 1;
 				};
+				System.out.println(path_array.get(path_array.size()-1));
+				path_array.remove(path_array.size()-1);
 			}
 		}
 		
 		depth--;
+
 		return 0;
 	}
 
--- src/maze_solving/node.java
+++ src/maze_solving/node.java
@@ -1,21 +1,34 @@
 package maze_solving;
 
-public class node {
+public class node implements Comparable<node> {
 	private int depth;
 	private int row;
 	private int column;
 	private boolean visited;
+	private int eval; 
+	private node parent;
 	
 	public node(int r, int c, int d){
 		depth = d;
 		row = r;
 		column = c;
 		visited = false;
+		eval = 999;
+		parent = null; 
 	}
 	
+	@Override
+	public int compareTo(node compareNode){
+		int compareEval = ((node) compareNode).getEval();
+		return this.eval - compareEval;
+		
+	}
 	
 	public void setDepth(int m){
 		depth =m;
+	}
+	public void setEval(int m){
+		eval = m;
 	}
 	
 	public void setRow(int m){
@@ -30,15 +43,23 @@ public class node {
 		visited = v;
 	}
 	
+	public void setParent(node c_node) {
+		parent = c_node;
+	}
+	
 	public int getDepth(){
 		return depth;
 	}
 	
-	public int getRow(){
+	public int getEval(){
+		return eval;
+	}
+	
+	public int y(){
 		return row;
 	}
 	
-	public int getColumn(){
+	public int x(){
 		return column;
 	}
 	
@@ -46,5 +67,8 @@ public class node {
 		return visited;
 	}
 	
-
+	public node getParent() {
+		return parent;
+	}
+	
 }
