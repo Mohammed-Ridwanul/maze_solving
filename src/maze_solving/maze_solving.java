48e0ec6d4f9ea2217ffeead573d40b8f8e15c63f
mohammed-ridwanul <mr3islam@uwaterloo.ca>
2016-07-06 14:56:06 -0400

    A* done.

--- src/maze_solving/maze_solving.java
+++ src/maze_solving/maze_solving.java
@@ -7,14 +7,18 @@ public class maze_solving {
 	public static void main(String[] args) {
 		dfs d = new dfs();
 		bfs b = new bfs();
+		a_star a = new a_star();
 		
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Which one? DFS =1, BFS = 2 ");
+		System.out.print("Which one? DFS =1, BFS = 2, A* = 3 ");
 		int search_type = Integer.parseInt(scanner.next());
+		
 		if(search_type == 1){
 			d.dfs_main();
 		} else if(search_type ==2){
 			b.bfs_main();
+		} else if(search_type ==3){
+			a.a_star_main();
 		}
 	}
 }
